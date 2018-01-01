52f70c64a570f09331fe55d8ce10377f69b029bf
Unknown <paul86677889@gmail.com>
2018-01-01 13:38:54 +0800

    final

--- module.js
+++ module.js
@@ -55,12 +55,6 @@
 		};
 	
 
-	// Module.prototype.nowSate =function(sate){
-	// 	console.log(222);
-	// 	return this.option.class[this.satePoint[sate]];
-	// 	console.log(this.option.class[this.satePoint[sate]]);
-	// };
-
 	
 
 		Module.prototype.init = function () {
@@ -71,13 +65,11 @@
 		var x = this.sate;
 		if ( this.option.openAtStart ===false ) {
 			this.sate = 2;//banner關閉時
-			document.getElementById('Btnch').innerHTML = '展開';
-			//按鈕文字變化
+			this.$btn.text( this.option.button.openText );//按鈕文字變化
 			 // this.addTransition();
 		}else{
 			this.sate = 0;//banner開啟時
-			document.getElementById('Btnch').innerHTML = '收合';
-			//按鈕文字變化
+			this.$btn.text( this.option.button.closeText );//按鈕文字變化
 			// this.addTransition();
 		}
 		
@@ -112,78 +104,98 @@
 		// obj [propName] 
 
 	Module.prototype.addTransition = function() {
-		if (!this.$ele.hasClass('transition') ) {
+		if ( ! this.$ele.hasClass('transition') ) {
 			this.$ele.addClass('transition');
-		}
+		};
 	};//判斷原ele是否有transition,如果沒有則加。
 
 	Module.prototype.toggle = function () {
+		 
 		 this.clearTimer();
+
 		 this.addTransition();
-		if ( this.sate === 2) {	
+
+		if ( this.sate === 2 ) {
 			this.open();
-		} else if ( this.sate === 0 ) {
-			 this.close();
-		}
-		if(this.sate === 0 || this.sate === 1 ){
-			document.getElementById('Btnch').innerHTML = '收合';
-			//JS修改HTML中間文字
-		}else{
-			document.getElementById('Btnch').innerHTML = '展開';
-			//JS修改HTML中間文字
-		};
+		} else if ( this.sate === 0 ) {	
+			this.close();
+		};	
 		this.timer = setInterval(this.option.whenTransition, 25);
+		// if(this.sate === 0 || this.sate === 1 ){
+		// 	document.getElementById('Btnch').innerHTML = '收合';
+		// 	//JS修改HTML中間文字
+		// }else{
+		// 	document.getElementById('Btnch').innerHTML = '展開';
+		// 	//JS修改HTML中間文字
+		// };	
 	};
-
-	Module.prototype.open = function () {
-		this.$ele.removeClass( this.nowSate(this.sate) ).addClass( this.nowSate(this.downSate()) );
-  		this.$ele.removeClass( this.nowSate(this.sate) ).addClass( this.nowSate(this.downSate()) );
-		return this.sate;
-	};//如何進行開的動作closed-opening-opened[2-3-0]
-
-
-	Module.prototype.close = function () {
-		this.$ele.removeClass( this.nowSate(this.sate) ).addClass( this.nowSate(this.goSate()) );
-		this.$ele.removeClass( this.nowSate(this.sate) ).addClass( this.nowSate(this.goSate()) );
-		return this.sate;			
-	};//如何進行關的動作opened-closing-closed[0-1-2]
-	
-	Module.prototype.goSate = function () {
-		if(this.sate > this.satePoint.lenght-1){
-			this.sate = 0;
-		}
-		this.sate ++;
-		return this.sate;//停止執行
-	};
-	Module.prototype.downSate = function () {
-		if(this.sate < this.satePoint.lenght-1){
-			this.sate = 2;
-		}
-		this.sate --;
-		return this.sate;//停止執行
-	};
-	// Module.prototype.clearTimer = function() {
-	// 	clearInterval(this.timer);
-	// 	clearTimeout(this.timer);
-	// };
-
 
 	Module.prototype.transitionEnd = function () {
-		if ( this.status === 1 ) {
-			this.$ele.removeClass( this.nowSate(this.sate) ).addClass( this.nowSate(this.goSate()) );
-		} else if ( this.status === 3 ) {
-				this.$ele.removeClass( this.nowSate(this.sate) ).addClass( this.nowSate(this.goSate()) );
+		if ( this.sate === 1 ) {
+			this.$ele.removeClass( this.nowSate(this.sate) ).addClass( this.nowSate(this.nextStatus()) );
+		} else if ( this.sate === 3 ) {
+			this.$ele.removeClass( this.nowSate(this.sate) ).addClass( this.nowSate(this.nextStatus()) );
 			}
 		 this.clearTimer();
 	};
+
+	//測試中
+
+
+	// Module.prototype.open = function () {
+	// 	this.$ele.removeClass( this.nowSate(this.sate) ).addClass( this.nowSate(this.downSate()) );
+ //  		this.$ele.removeClass( this.nowSate(this.sate) ).addClass( this.nowSate(this.downSate()) );
+	// 	return this.sate;
+	// };//如何進行開的動作closed-opening-opened[2-3-0]
+
+
+	// Module.prototype.close = function () {
+	// 	this.$ele.removeClass( this.nowSate(this.sate) ).addClass( this.nowSate(this.goSate()) );
+	// 	this.$ele.removeClass( this.nowSate(this.sate) ).addClass( this.nowSate(this.goSate()) );
+	// 	return this.sate;			
+	// };//如何進行關的動作opened-closing-closed[0-1-2]
+	
+
+	//測試中
+	Module.prototype.nextStatus = function () {
+		this.sate++;
+		if ( this.sate > this.satePoint.length-1 ) {
+			this.sate = 0;
+		}
+		return this.sate;
+	};
+
+	Module.prototype.open = function () {
+		this.$ele.removeClass( this.nowSate(this.sate) ).addClass( this.nowSate(this.nextStatus()) );
+		this.$btn.text(this.option.button.closeText);
+	// this.$ele.removeClass( this.nowSate(this.sate) ).addClass( this.nowSate(this.nextStatus()) );
+	};
+
+	Module.prototype.close = function () {
+		this.$ele.removeClass( this.nowSate(this.sate) ).addClass( this.nowSate(this.nextStatus()) );
+		this.$btn.text(this.option.button.openText);
+	// this.$ele.removeClass( this.nowSate(this.sate) ).addClass( this.nowSate(this.nextStatus()) );
+	};
+
+	// Module.prototype.goSate = function () {
+	// 	if(this.sate > this.satePoint.lenght-1){
+	// 		this.sate = 0;
+	// 	}
+	// 	this.sate ++;
+	// 	return this.sate;//停止執行
+	// };
+	// Module.prototype.downSate = function () {
+	// 	if(this.sate < this.satePoint.lenght-1){
+	// 		this.sate = 2;
+	// 	}
+	// 	this.sate --;
+	// 	return this.sate;//停止執行
+	// };
 	
 	Module.prototype.clearTimer = function() {
 		clearInterval(this.timer);
 		clearTimeout(this.timer);
 	};
-
-
-
 
 
 	$.fn[ModuleName] = function ( method, options ) {
@@ -207,7 +219,7 @@
 				module.init();
 				
 				module.$btn.on('click', function() {
-					module.toggle();
+					module.toggle(0);
 				});
 
 				module.$ele.on(module.transitionEndEvent, function(e, ignore) {
